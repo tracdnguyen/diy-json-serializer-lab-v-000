e970409454641b990c60dc2ad9e27250bc8dd33e
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-24 05:39:38 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,7 +2,7 @@ class ProductSerializer
   def self.serialize(product)
     serialized_product = '{'
 
-    serialized_product += '"name: "' + product.name + '", '
+    serialized_product += '"name: ' + product.name + '", '
     serialized_product += '"price: "' + product.price.to_s + ', '
     serialized_product += '"inventory: "' + product.inventory.to_s + ', '
     serialized_product += '"description: "' + product.description + '" '
