6d47143a691dcc637f7951c7d3509a79ce6a52dd
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-24 05:30:15 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -3,7 +3,7 @@ class ProductSerializer
     serialized_product = '{'
 
     serialized_product += '"name: "' + product.name + '", '
-    serialized_product += '"price: "' + product.price.to_s + ', '
+    serialized_product += '"price: ' + product.price.to_s + '", '
     serialized_product += '"inventory: "' + product.inventory.to_s + ', '
     serialized_product += '"description: "' + product.description + '" '
 
